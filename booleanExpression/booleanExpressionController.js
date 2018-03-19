8efba43d7b5bb7b02801f3ce5fd2bf1ee6260ce4
Rishi Goel <rishigoel@v1020-wn-117-157.campus-dynamic.uwaterloo.ca>
2018-03-19 10:50:45 -0400

    made changes for refactoring guidelines

--- booleanExpression/booleanExpressionController.js
+++ booleanExpression/booleanExpressionController.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const VerifyToken = require('./../auth/VerifyToken')
+const constants = require('./../constants')
 
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(bodyParser.json())
@@ -13,33 +14,33 @@ var BooleanExpression = require('./booleanExpression')
 // RETURNS ALL THE BOOLEAN EXPRESSIONS IN THE DATABASE
 router.get('/', (req, res) => {
     User.find({}, (err, users) => {
-        if (err) return res.status(500).send("There was a problem finding the users.")
-        res.status(200).send(users)
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem finding the users.")
+        res.status(constants.OK_STATUS_CODE).send(users)
     })
 })
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:id', (req, res) => {
     User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.")
-        if (!user) return res.status(404).send("No user found.")
-        res.status(200).send(user)
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem finding the user.")
+        if (!user) return res.status(constants.ERROR_STATUS_CODE).send("No user found.")
+        res.status(constants.OK_STATUS_CODE).send(user)
     })
 })
 
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', (req, res) => {
     User.findByIdAndRemove(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem deleting the user.")
-        res.status(200).send("User: " + user.name + " was deleted.")
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem deleting the user.")
+        res.status(constants.OK_STATUS_CODE).send("User: " + user.name + " was deleted.")
     })
 })
 
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', (req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
-        if (err) return res.status(500).send("There was a problem updating the user.")
-        res.status(200).send(user)
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem updating the user.")
+        res.status(constants.OK_STATUS_CODE).send(user)
     })
 })
 
--- user/UserController.js
+++ user/UserController.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 const VerifyToken = require('./../auth/VerifyToken')
+const constants = require('./../constants')
 
 router.use(bodyParser.urlencoded({ extended: true }))
 router.use(bodyParser.json())
@@ -15,41 +16,41 @@ router.post('/', (req, res) => {
             email : req.body.email,
             password : req.body.password
         }, (err, user) => {
-            if (err) return res.status(500).send("There was a problem adding the information to the database.")
-            res.status(200).send(user)
+            if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem adding the information to the database.")
+            res.status(constants.OK_STATUS_CODE).send(user)
         })
 })
 
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/', VerifyToken, (req, res) => {
     User.find({}, (err, users) => {
-        if (err) return res.status(500).send("There was a problem finding the users.")
-        res.status(200).send(users)
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem finding the users.")
+        res.status(constants.OK_STATUS_CODE).send(users)
     })
 })
 
 // GETS A SINGLE USER FROM THE DATABASE
 router.get('/:id', VerifyToken, (req, res) => {
     User.findById(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem finding the user.")
-        if (!user) return res.status(404).send("No user found.")
-        res.status(200).send(user)
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem finding the user.")
+        if (!user) return res.status(constants.ERROR_STATUS_CODE).send("No user found.")
+        res.status(constants.OK_STATUS_CODE).send(user)
     })
 })
 
 // DELETES A USER FROM THE DATABASE
 router.delete('/:id', VerifyToken, (req, res) => {
     User.findByIdAndRemove(req.params.id, function (err, user) {
-        if (err) return res.status(500).send("There was a problem deleting the user.")
-        res.status(200).send("User: " + user.name + " was deleted.")
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem deleting the user.")
+        res.status(constants.OK_STATUS_CODE).send("User: " + user.name + " was deleted.")
     })
 })
 
 // UPDATES A SINGLE USER IN THE DATABASE
 router.put('/:id', VerifyToken, (req, res) => {
     User.findByIdAndUpdate(req.params.id, req.body, {new: true}, function (err, user) {
-        if (err) return res.status(500).send("There was a problem updating the user.")
-        res.status(200).send(user)
+        if (err) return res.status(constants.INVALID_STATUS_CODE).send("There was a problem updating the user.")
+        res.status(constants.OK_STATUS_CODE).send(user)
     })
 })
 
